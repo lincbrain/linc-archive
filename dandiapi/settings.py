719a0b8d17b87fafa0675066c0c1be228f5d3199
Aaron Kanzer <aaronkanzer@Aarons-MacBook-Pro.local>
2024-01-25 09:51:51 -0500

    Configure values for staging only

--- dandiapi/settings.py
+++ dandiapi/settings.py
@@ -198,13 +198,11 @@ class HerokuProductionConfiguration(DandiMixin, HerokuProductionBaseConfiguratio
     if 'ALLOWED_HOSTS' not in globals():
         ALLOWED_HOSTS = []
 
+    # api.lincbrain.org
     ALLOWED_HOSTS += [
         'linc-staging-terraform-83d11b79c499.herokuapp.com',
-        'api.lincbrain.com',
-        'api.lincbrain.org',
         'staging-api.lincbrain.org'
     ]
-    
 
 
 # NOTE: The staging configuration uses a custom OAuth toolkit `Application` model
