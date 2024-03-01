fbaa3f3ad000a2f1505ef8bf529fb2d9e508f5d0
Aaron Kanzer <aaronkanzer@Aarons-MacBook-Pro.local>
2024-02-29 20:20:06 -0500

    cookies working

--- dandiapi/api/views/private_s3_permissions.py
+++ dandiapi/api/views/private_s3_permissions.py
@@ -7,6 +7,8 @@ from rest_framework.decorators import api_view, parser_classes, permission_class
 from rest_framework.parsers import JSONParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+
 
 from dandiapi.api.utils import get_presigned_cookies
 import rsa
@@ -19,35 +21,164 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from botocore.signers import CloudFrontSigner
+#
+#
+# def rsa_signer(message):
+#     import os
+#     path = f'{os.getcwd()}/dandiapi/api/privkey.pem'
+#     with open(path, 'rb') as key_file:
+#         private_key = serialization.load_pem_private_key(
+#             key_file.read(),
+#             password=None,
+#             backend=default_backend()
+#         )
+#     return private_key.sign(message, padding.PKCS1v15(), hashes.SHA1())
+#
+#
+# def get_presigned_url():
+#     key_id = 'K350HZY8RS1FBD'  # pkcs1 -- lincbrain AWS
+#     object_url = 'https://neuroglancer.lincbrain.org/*'
+#     expire_date = datetime.datetime(2025, 10, 12)
+#
+#     cloudfront_signer = CloudFrontSigner(key_id, rsa_signer)
+#
+#     # Create a signed url that will be valid until the specific expiry date
+#     # provided using a canned policy.
+#     signed_url = cloudfront_signer.generate_presigned_url(
+#         object_url, date_less_than=expire_date)
+#     return signed_url
+#
+# from urllib.parse import urlparse, parse_qs
+#
+# def _replace_unsupported_chars(some_str):
+#     """Replace unsupported chars: '+=/' with '-_~'"""
+#     return some_str.replace("+", "-") \
+#         .replace("=", "_") \
+#         .replace("/", "~")
+#
+# def transform_presigned_url_to_cookies(presigned_url):
+#     # Parse the URL
+#     parsed_url = urlparse(presigned_url)
+#     query_params = parse_qs(parsed_url.query)
+#
+#     # Transform the signature to its original base64 format
+#     # Replace URL-safe base64 characters to standard base64 characters
+#     signature = query_params['Signature'][0]
+#     good_signature = _replace_unsupported_chars(signature)
+#
+#     # Extract the relevant parts for the cookies
+#     cookies = {
+#         'CloudFront-Expires': query_params['Expires'][0],
+#         'CloudFront-Signature': good_signature,
+#         'CloudFront-Key-Pair-Id': query_params['Key-Pair-Id'][0]
+#     }
+#
+#     return cookies
+
+import time
+import json
+import base64
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
 
 
-def rsa_signer(message):
-    import os
-    path = f'{os.getcwd()}/dandiapi/api/privkey.pem'
-    with open(path, 'rb') as key_file:
-        private_key = serialization.load_pem_private_key(
-            key_file.read(),
-            password=None,
-            backend=default_backend()
-        )
-    return private_key.sign(message, padding.PKCS1v15(), hashes.SHA1())
+def _replace_unsupported_chars(some_str):
+    """Replace unsupported chars: '+=/' with '-_~'"""
+    return some_str.replace("+", "-") \
+        .replace("=", "_") \
+        .replace("/", "~")
 
 
-def get_presigned_url():
-    key_id = 'K350HZY8RS1FBD'  # pkcs1 -- lincbrain AWS
-    object_url = 'https://neuroglancer.lincbrain.org/sample_file.txt'
-    expire_date = datetime.datetime(2025, 10, 12)
-
-    cloudfront_signer = CloudFrontSigner(key_id, rsa_signer)
-
-    # Create a signed url that will be valid until the specific expiry date
-    # provided using a canned policy.
-    signed_url = cloudfront_signer.generate_presigned_url(
-        object_url, date_less_than=expire_date)
-    return signed_url
+def _in_an_hour():
+    """Returns a UTC POSIX timestamp for one hour in the future"""
+    return int(time.time()) + (60*60)
 
 
-import datetime
+def rsa_signer(message, key):
+    """
+    Sign a message using RSA private key with PKCS#1 v1.5 padding and SHA-1 hash.
+
+    :param message: The message to sign
+    :param key: RSA private key in PEM format
+    :return: The signature
+    """
+    private_key = load_pem_private_key(
+        key,
+        password=None,  # Replace with password if the private key is encrypted
+    )
+
+    # Updated signing process
+    signature = private_key.sign(
+        message,
+        padding.PKCS1v15(),
+        hashes.SHA1()
+    )
+
+    return signature
+
+
+# def rsa_signer(message, key):
+#     """
+#     Based on https://boto3.readthedocs.io/en/latest/reference/services/cloudfront.html#examples
+#     """
+#     private_key = serialization.load_pem_private_key(
+#         key,
+#         password=None,
+#         backend=default_backend()
+#     )
+#     signer = private_key.signer(padding.PKCS1v15(), hashes.SHA1())
+#     signer.update(message)
+#     return signer.finalize()
+
+
+def generate_policy_cookie(url):
+    """Returns a tuple: (policy json, policy base64)"""
+
+    policy_dict = {
+        "Statement": [
+            {
+                "Resource": url,
+                "Condition": {
+                    "DateLessThan": {
+                        "AWS:EpochTime": _in_an_hour()
+                    }
+                }
+            }
+        ]
+    }
+
+    # Using separators=(',', ':') removes seperator whitespace
+    policy_json = json.dumps(policy_dict, separators=(",", ":"))
+
+    policy_64 = str(base64.b64encode(policy_json.encode("utf-8")), "utf-8")
+    policy_64 = _replace_unsupported_chars(policy_64)
+    return policy_json, policy_64
+
+
+def generate_signature(policy, key):
+    """Creates a signature for the policy from the key, returning a string"""
+    sig_bytes = rsa_signer(policy.encode("utf-8"), key)
+    sig_64 = _replace_unsupported_chars(str(base64.b64encode(sig_bytes), "utf-8"))
+    return sig_64
+
+
+def generate_cookies(policy, signature, cloudfront_id):
+    """Returns a dictionary for cookie values in the form 'COOKIE NAME': 'COOKIE VALUE'"""
+    return {
+        "CloudFront-Policy": policy,
+        "CloudFront-Signature": signature,
+        "CloudFront-Key-Pair-Id": cloudfront_id
+    }
+
+
+def generate_signed_cookies(url, cloudfront_id, key):
+    policy_json, policy_64 = generate_policy_cookie(url)
+    signature = generate_signature(policy_json, key)
+    return generate_cookies(policy_64, signature, cloudfront_id)
+
 
 if TYPE_CHECKING:
     from django.http.response import HttpResponseBase
@@ -64,19 +195,24 @@ if TYPE_CHECKING:
 @parser_classes([JSONParser])
 @permission_classes([IsAuthenticated])
 def presigned_cookie_s3_cloudfront_view(request: Request) -> HttpResponseBase:
+    import os
+    path = f'{os.getcwd()}/dandiapi/api/privkey.pem'
+    key_id = 'K350HZY8RS1FBD'  # pkcs1 -- lincbrain AWS
+    object_url = 'https://neuroglancer.lincbrain.org/*'
+    with open(path, "rb") as f:
+        cookies = generate_signed_cookies(object_url, key_id, f.read())
 
-
-    response_data = {"message": get_presigned_url()}
+    # print(generate_curl_cmd(args.url, cookies))
+    # cookies = transform_presigned_url_to_cookies(get_presigned_url())
+    response_data = {"message": cookies}
     response = Response(response_data)
-
-    cookies = {}
     for cookie_name, cookie_value in cookies.items():
-        response.set_cookie(
-            key=cookie_name,
-            value=cookie_value,
-            secure=True,
-            httponly=True,
-            domain=".lincbrain.org"
-        )
+            response.set_cookie(
+                key=cookie_name,
+                value=cookie_value,
+                secure=True,
+                httponly=True,
+                domain=".lincbrain.org"
+            )
 
     return response
