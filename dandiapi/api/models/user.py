a9e40f7abe0d16fd6d3822178625527d4ff1cffb
Aaron Kanzer <aaronkanzer@Aarons-MacBook-Pro.local>
2024-07-07 13:59:56 -0700

    Include field for webknossos creds

--- dandiapi/api/models/user.py
+++ dandiapi/api/models/user.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
@@ -16,4 +17,11 @@ class UserMetadata(TimeStampedModel):
     status = models.CharField(choices=Status.choices, default=Status.INCOMPLETE, max_length=10)
     questionnaire_form = models.JSONField(blank=True, null=True)
     rejection_reason = models.TextField(blank=True, default='', max_length=1000)
+    webknossos_credential = models.CharField(max_length=128, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        # Ensure the password is hashed before saving if it is not null or empty
+        if self.password and not self.password.startswith('pbkdf2_sha256'):
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
 
