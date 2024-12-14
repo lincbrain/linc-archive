a1ba8bb6cd05020ae4bf445621e4e2e298094276
Aaron Kanzer <aaronkanzer@Aarons-MacBook-Pro.local>
2024-12-14 15:29:02 -0500

    remove call that doesnt exist for emabrgoed blob in migrations

--- dandiapi/api/migrations/0014_auditrecord_and_more.py
+++ dandiapi/api/migrations/0014_auditrecord_and_more.py
@@ -18,63 +18,6 @@ def remove_access_fields(apps, _):
         details=RawSQL("details #- '{metadata, access}'", [])
     )
 
-def migrate_embargoed_asset_blobs(apps, _):
-    Asset = apps.get_model('api.Asset')
-    AssetBlob = apps.get_model('api.AssetBlob')
-    EmbargoedAssetBlob = apps.get_model('api.EmbargoedAssetBlob')
-
-    def migrate_embargoed_blob(embargoed_blob):
-        # Check if the blob we care about already exists (possibly under a different blob_id due to
-        # de-duplication).
-        # This will handle the following cases:
-        #   1. This asset is part of an "asset chain", where multiple assets point to the same blob
-        #   2. This blob this asset points to exists across multiple embargoed dandisets under
-        #       different blob_ids, due to the lack of cross-dandiset embargo de-duplication.
-        #   3. This blob this asset points to already exists as a normal AssetBlob, due to the lack
-        #       of de-duplication between open and embargoed dandisets. This is essentially the same
-        #       as the above case, but between an embargoed and open dandiset, instead of two
-        #       embargoed dandisets.
-        #
-        # In case #3, the asset will effectively be unembargoed.
-        existing_blob = AssetBlob.objects.filter(
-            etag=embargoed_blob.etag, size=embargoed_blob.size
-        ).first()
-        if existing_blob:
-            existing_blob.download_count += embargoed_blob.download_count
-            existing_blob.save()
-            return existing_blob
-
-        blob_id = str(embargoed_blob.blob_id)
-        new_blob_location = f'blobs/{blob_id[0:3]}/{blob_id[3:6]}/{blob_id}'
-        return AssetBlob.objects.create(
-            embargoed=True,
-            blob=new_blob_location,
-            blob_id=embargoed_blob.blob_id,
-            created=embargoed_blob.created,
-            modified=embargoed_blob.modified,
-            sha256=embargoed_blob.sha256,
-            etag=embargoed_blob.etag,
-            size=embargoed_blob.size,
-            download_count=embargoed_blob.download_count,
-        )
-
-    # For each relevant asset, create a new asset blob with embargoed=True,
-    # and point the asset to that
-    embargoed_assets = Asset.objects.filter(embargoed_blob__isnull=False).select_related(
-        'embargoed_blob'
-    )
-    for asset in embargoed_assets.iterator():
-        asset.blob = migrate_embargoed_blob(asset.embargoed_blob)
-        asset.embargoed_blob = None
-        asset.save()
-    assert not Asset.objects.filter(embargoed_blob__isnull=False).exists()  # noqa: S101
-
-    # Finally, handle orphaned EmbargoedAssetBlobs. Since we've already taken care of all assets
-    # that point to embargoed blobs, we can migrate the remaining embargoed blobs without issue.
-    embargoed_blobs = EmbargoedAssetBlob.objects.iterator()
-    for embargoed_blob in embargoed_blobs:
-        migrate_embargoed_blob(embargoed_blob)
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -116,5 +59,4 @@ class Migration(migrations.Migration):
             model_name='assetpath',
             constraint=models.CheckConstraint(check=models.Q(('path__endswith', '/'), ('path__startswith', '/'), _connector='OR', _negated=True), name='consistent-slash'),
         ),
-        migrations.RunPython(migrate_embargoed_asset_blobs),
     ]
