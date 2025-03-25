dandi/dandi-archive

Run `eslint-plugin-vuetify` on more components

vue3/src/views/DandisetLandingView/DandisetValidationErrors.vue
@@ -8,14 +8,13 @@
       <v-menu
         :nudge-width="200"
       >
-        <template #activator="{ on: menu, attrs }">
-          <v-tooltip bottom>
-            <template #activator="{ on: tooltip }">
+        <template #activator="{ props: menuProps }">
+          <v-tooltip location="bottom">
+            <template #activator="{ props: tooltipProps }">
               <v-card
-                class="amber lighten-5 no-text-transform"
-                outlined
-                v-bind="attrs"
-                v-on="{ ...tooltip, ...menu }"
+                class="bg-amber-lighten-5 no-text-transform"
+                variant="outlined"
+                v-on="{ ...menuProps, ...tooltipProps }"
               >
                 <v-row class="align-center px-4">
                   <v-col
@@ -57,8 +56,8 @@
     <!-- Version Validation Errors Button -->
     <v-card
       v-if="dandiset.version_validation_errors.length"
-      class="my-2 px-1 amber lighten-5 no-text-transform"
-      outlined
+      class="my-2 px-1 bg-amber-lighten-5 no-text-transform"
+      variant="outlined"
       @click="openErrorDialog('metadata')"
     >
       <v-row class="align-center px-4">
@@ -86,8 +85,8 @@
     <!-- Asset Validation Errors Button -->
     <v-card
       v-if="numAssetValidationErrors"
-      class="my-2 px-1 amber lighten-5 no-text-transform"
-      outlined
+      class="my-2 px-1 bg-amber-lighten-5 no-text-transform"
+      variant="outlined"
       @click="openErrorDialog('assets')"
     >
       <v-row class="align-center px-4">

lincbrain/linc-archive

vue3/src/views/DandisetLandingView/DandisetValidationErrors.vue
@@ -8,14 +8,13 @@
       <v-menu
         :nudge-width="200"
       >
-        <template #activator="{ on: menu, attrs }">
-          <v-tooltip bottom>
-            <template #activator="{ on: tooltip }">
+        <template #activator="{ props: menuProps }">
+          <v-tooltip location="bottom">
+            <template #activator="{ props: tooltipProps }">
               <v-card
-                class="amber lighten-5 no-text-transform"
-                outlined
-                v-bind="attrs"
-                v-on="{ ...tooltip, ...menu }"
+                class="bg-amber-lighten-5 no-text-transform"
+                variant="outlined"
+                v-bind="{ ...menuProps, ...tooltipProps }"
               >
                 <v-row class="align-center px-4">
                   <v-col
@@ -57,8 +56,8 @@
     <!-- Version Validation Errors Button -->
     <v-card
       v-if="dandiset.version_validation_errors.length"
-      class="my-2 px-1 amber lighten-5 no-text-transform"
-      outlined
+      class="my-2 px-1 bg-amber-lighten-5 no-text-transform"
+      variant="outlined"
       @click="openErrorDialog('metadata')"
     >
       <v-row class="align-center px-4">
@@ -86,8 +85,8 @@
     <!-- Asset Validation Errors Button -->
     <v-card
       v-if="numAssetValidationErrors"
-      class="my-2 px-1 amber lighten-5 no-text-transform"
-      outlined
+      class="my-2 px-1 bg-amber-lighten-5 no-text-transform"
+      variant="outlined"
       @click="openErrorDialog('assets')"
     >
       <v-row class="align-center px-4">